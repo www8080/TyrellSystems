bb08b63568b9d63926754ba002eeed2b98334547
www8080 <paulsiew@hotmail.com>
2020-04-27 17:33:01 +0800

    Updated script with Readme/Usage instruction
    Updated script with Readme/Usage instruction

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
-Tyrell Systems Programming Test
+# Tyrell Systems Programming Test - PHP playing cards distribute system
+======================================================================
 
-*PHP playing cards distribute system*
+## README:
 
-#README:
-
-# Overview
+## Overview
 
 - This system capture input [Number of people] and playing cards will be given out to these n(number) of people. 
 - Total 52 cards containing 1-13 of each Spade(S), Heart(H), Diamond(D), Club(C) will be given to n people randomly.
@@ -14,7 +13,16 @@ Tyrell Systems Programming Test
 		-	If 52 cards, 52 people. Each people will receive 1 card each.
 		-	If 52 cards, 53 people. Each people will receive 1 card each, except the last people(53th person) will not receive card because the card was empty.
 
+## Folder Structure
+	'/path/to/your/www/yourFolder'/playingCard.php
+
+### playingCard.php
+	This file will contains PHP, HTML and JQuery code
+
 ## Usage
 1.	User will key-in the total of people in the input box[Number of people].
 2. 	Click on the Submit button to generate the result.
-3.	The page contains of a table with 2 colume, left colume 'debugging'
+3.	The page contains of a table with 2 colume, 
+			- left colume 'debugging' containing information of the Original cards in order.
+			- Right colume containing the input box[Number of people] and the submit button, and the cards distribution result shows below. Last row will show 'Total card left'.
+			
--- playingCard.php
+++ playingCard.php
@@ -1,4 +1,36 @@
-﻿<?
+﻿<!--
+# Tyrell Systems Programming Test - PHP playing cards distribute system
+======================================================================
+
+## README:
+
+## Overview
+
+- This system capture input [Number of people] and playing cards will be given out to these n(number) of people. 
+- Total 52 cards containing 1-13 of each Spade(S), Heart(H), Diamond(D), Club(C) will be given to n people randomly.
+-	Card format (Spade = S, Heart = H, Diamond = D, Club = C), (Card 2 to 9 are, as it is, 1=A,10=X,11=J,12=Q,13=K).
+- Each people will receive number of cards evenly in a 'First Come First Serve' basis 
+	-	For example scenario below,
+		-	If 52 cards, 52 people. Each people will receive 1 card each.
+		-	If 52 cards, 53 people. Each people will receive 1 card each, except the last people(53th person) will not receive card because the card was empty.
+
+## Folder Structure
+	'/path/to/your/www/yourFolder'/playingCard.php
+
+### playingCard.php
+	This file will contains PHP, HTML and JQuery code
+
+## Usage
+1.	User will key-in the total of people in the input box[Number of people].
+2. 	Click on the Submit button to generate the result.
+3.	The page contains of a table with 2 colume, 
+			- left colume 'debugging' containing information of the Original cards in order.
+			- Right colume containing the input box[Number of people] and the submit button, and the cards distribution result shows below. Last row will show 'Total card left'.
+
+//END OF README
+-->
+
+<?
 $numOfPeople	= floor($_POST['numOfPeople']);
 $cardTypes		= array('S','H','D','C');
 $cards				= array('A',2,3,4,5,6,7,8,9,'X','J','Q','K');
@@ -102,7 +134,6 @@ echo "<br>numOfPeople:". $numOfPeople."<br>";
 							<button type="button">Submit</button>
 					</form>
 					<?
-						//<input type="submit" name="Action" value="Submit">
 						//Dealing Cards to N people
 						echo "<b><u>Total of People:</u> <font color='#008000'> ". $numOfPeople."</font></b><br>";
 					?>
@@ -156,7 +187,6 @@ echo "<br>numOfPeople:". $numOfPeople."<br>";
 							echo "<font color='#008000'>";
 								print_r($myHandSepByComma);
 							echo "</font>";
-							//if (count($myHand))
 							unset($myHand);
 						}
 					?>
